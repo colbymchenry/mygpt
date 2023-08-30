19f82eea467de54c02574b6aa48112920bc3013a
Colby McHenry <me@colbymchenry.com>
2023-08-29 20:55:54 -0500

    Fix width overflow and disable mobile zoom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/contexts/ThemeContext';
 import '@/styles/fonts.css';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,9 @@ export default function App({ Component, pageProps }: AppProps) {
           <div className="flex flex-col">
             <Navbar />
             <div className="page-width py-6">
+              <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+              </Head>
               <Component {...pageProps} />
             </div>
           </div>
